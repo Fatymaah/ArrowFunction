2660624858593627240e78017d8784a3e7b21177
Fatymaah <fatymahmohamed@gmail.com>
2018-06-23 08:54:43 +0300

    Arrow

--- arrow.js
+++ arrow.js
@@ -1,9 +1,9 @@
-var add (v,s) => (v+s);
-console.log(7,2)
+var add = (v,s) => (v+s);
+console.log(add(7,2))
 
 
-var subtract (f,a) => (f-a);
-console.log(70,30)
+var subtract = (f,a) => (f-a);
+console.log(subtract(70,30))
 
-var multiply (r,w) => (r*w);
-console.log(7,3)
+var multiply = (r,w) => (r*w);
+console.log(multiply(7,3))
